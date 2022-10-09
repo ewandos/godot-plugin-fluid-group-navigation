eaeff49df5744cdf503897344d56280b33fef44b
Philip Ewert <developer@philipewert.com>
2022-10-09 15:35:07 +0200

    use the navigation server directly without the navigationagent

--- PathRenderer.gd
+++ PathRenderer.gd
@@ -5,9 +5,9 @@ extends Node2D
 func _ready():
 	units = get_tree().get_nodes_in_group("unit")
 	for unit in units:
-		unit.agent.path_changed.connect(queue_redraw)
+		unit.connect("path_changed", queue_redraw)
 
 func _draw():
 	for unit in units:
-		if (unit.agent.get_nav_path().size() < 2): return
-		draw_polyline(unit.agent.get_nav_path(), Color.MAGENTA, 5)
+		if unit.result.path.size() < 2: return
+		draw_polyline(unit.result.path, Color.MAGENTA, 5)
--- Unit.gd
+++ Unit.gd
@@ -2,34 +2,29 @@ extends CharacterBody2D
 class_name Unit
 
 @export var color: Color
+signal path_changed
 var is_selected := false
+var result := NavigationPathQueryResult2D.new()
 
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var agent: NavigationAgent2D = $NavigationAgent2d
 
 func _ready() -> void:
-	agent.velocity_computed.connect(on_velocity_computed)
-	agent.path_changed.connect(on_path_changed)
-	agent.target_reached.connect(on_target_reached)
+	pass
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and is_selected:
-		agent.set_target_location(get_viewport().get_mouse_position())
+		var parameter := NavigationPathQueryParameters2D.new()
+		parameter.path_postprocessing = NavigationPathQueryParameters2D.PATH_POSTPROCESSING_CORRIDORFUNNEL
+		parameter.start_position = position
+		parameter.target_position = get_viewport().get_mouse_position()
+		parameter.map = NavigationServer2D.get_maps()[0]
+		
+		NavigationServer2D.query_path(parameter, result)
+		print(result.path)
+		path_changed.emit()
 		
 func _physics_process(delta: float) -> void:
-	var next_location = agent.get_next_location()
-	var v = (next_location - global_position).normalized()
-	agent.set_velocity(v)
-
-func on_velocity_computed(safe_velocity: Vector2) -> void:
-	position += safe_velocity
-
-func on_path_changed() -> void:
 	pass
-
-func on_target_reached() -> void:
-	print("reached goal")
-	get_tree().quit()
 
 func select() -> void:
 	sprite.modulate = color
