20db4a8d74fc18359ffb861afd4731713c21bc4c
Philip Ewert <developer@philipewert.com>
2022-10-19 21:49:57 +0200

    switched to fetching path from navigation server

--- Unit.gd
+++ Unit.gd
@@ -8,29 +8,34 @@ var is_selected := false
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var agent: NavigationAgent2D = $NavigationAgent2d
 
+var result := []
+
 func _ready() -> void:
-	agent.velocity_computed.connect(on_velocity_computed)
-	agent.path_changed.connect(on_path_changed)
-	agent.target_reached.connect(on_target_reached)
+	pass
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed and is_selected:
-		agent.set_target_location(get_viewport().get_mouse_position())
+		var parameter := NavigationPathQueryParameters2D.new()
+		parameter.path_postprocessing = NavigationPathQueryParameters2D.PATH_POSTPROCESSING_CORRIDORFUNNEL
+		parameter.start_position = position
+		parameter.target_position = get_viewport().get_mouse_position()
+		parameter.map = NavigationServer2D.get_maps()[0]
+		var query_result := NavigationPathQueryResult2D.new()
+		NavigationServer2D.query_path(parameter, query_result)
+		result = query_result.get_path()
+		result.remove_at(0)
+		print(result)
 		
 func _physics_process(delta: float) -> void:
-	if (agent.get_target_location() == Vector2.ZERO): return
-	var next_location = agent.get_next_location()
-	var v = (next_location - global_position).normalized()
-	agent.set_velocity(v * speed)
-
-func on_velocity_computed(safe_velocity: Vector2) -> void:
-	position += safe_velocity
-
-func on_path_changed() -> void:
-	pass
-
-func on_target_reached() -> void:
-	print("reached goal")
+	if (result.size() == 0): return
+	var next_location = result[0]
+	var distance = global_position.distance_to(next_location)
+	
+	if (distance <= 10):
+		result.remove_at(0)
+	
+	var v = (next_location - global_position).normalized() * speed
+	position += v
 
 func select() -> void:
 	sprite.modulate = color
