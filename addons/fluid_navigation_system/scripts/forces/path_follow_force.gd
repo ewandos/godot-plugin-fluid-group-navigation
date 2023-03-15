6e9201d43a1cfadbdac40755cbe2eeee63560362
Philip Ewert <developer@philipewert.com>
2023-03-15 14:26:21 +0100

    added offset handling

--- addons/fluid_navigation_system/scripts/forces/path_follow_force.gd
+++ addons/fluid_navigation_system/scripts/forces/path_follow_force.gd
@@ -8,14 +8,20 @@ func calculate_force(agent: FluidAgentNavigation) -> Vector2:
 
 	if agent.path.size() == 0: return Vector2.ZERO
 
-	var distance = agent.global_position.distance_to(agent.path[0])
+	var oriented_path_offset := agent.path_offset
+	if agent.path.size() > 1:
+		var path_orientation: Vector2 = agent.path[0].direction_to(agent.path[1])
+		var orientation_angle := path_orientation.angle()
+		oriented_path_offset = agent.path_offset.rotated(orientation_angle)
+
+	var distance = agent.global_position.distance_to(agent.path[0] + oriented_path_offset)
 
 	if (distance <= agent.agent_attributes.path_stopping_distance):
 		agent.path.remove_at(0)
 
 	if agent.path.size() == 0: return Vector2.ZERO
 
-	steering_force = agent.global_position.direction_to(agent.path[0])
+	steering_force = agent.global_position.direction_to(agent.path[0] + oriented_path_offset)
 	steering_force *= agent.agent_attributes.max_speed
 
 	return agent.compute_force_to_steer_to_velocity(steering_force)
